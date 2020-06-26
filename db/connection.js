8973689e225f3062091a17102c0a9f367e8a9bc0
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-06-26 15:39:29 +0530

    changed mongo url to mlab url

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
+// const url = `mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
+
+const url = 'mongodb://expense-tracker:expense-tracker0018@ds061158.mlab.com:61158/expense-tracker'
 
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true }, (err,conn) => {
     if (err) {
